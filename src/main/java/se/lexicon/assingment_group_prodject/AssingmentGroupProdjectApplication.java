8ddc19412511d6a6c52dd13738c633691685a34b
Dave5535 <115458922+Dave5535@users.noreply.github.com>
2023-02-02 09:56:13 +0100

    Revert "Repository not finished"

--- src/main/java/se/lexicon/assingment_group_prodject/AssingmentGroupProdjectApplication.java
+++ src/main/java/se/lexicon/assingment_group_prodject/AssingmentGroupProdjectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AssignmentGroupProjectApplication {
+public class AssingmentGroupProdjectApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AssignmentGroupProjectApplication.class, args);
+        SpringApplication.run(AssingmentGroupProdjectApplication.class, args);
     }
 
 }
--- src/main/java/se/lexicon/assingment_group_prodject/Repository/RecipeInstructionRepository.java
+++
@@ -1,8 +0,0 @@
-package se.lexicon.assingment_group_prodject.Repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import se.lexicon.assingment_group_prodject.entity.RecipeInstruction;
-@Repository
-public interface RecipeInstructionRepository extends CrudRepository<RecipeInstruction,Integer> {
-}
