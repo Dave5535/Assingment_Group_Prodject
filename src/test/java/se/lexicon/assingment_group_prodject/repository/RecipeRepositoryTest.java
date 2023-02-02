df255c0c758a8e214e496c8b632a137a6e4e1282
Son Nghiem <nghiemminhson@gmail.com>
2023-02-02 10:50:54 +0100

    1. RecipeRepository methods with query creation were implemented and tested.

---
+++ src/test/java/se/lexicon/assingment_group_prodject/repository/RecipeRepositoryTest.java
@@ -0,0 +1,8 @@
+package se.lexicon.assingment_group_prodject.repository;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class RecipeRepositoryTest {
+
+}
